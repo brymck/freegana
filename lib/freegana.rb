295bfa917f23a79992719930ce57adc704f9b916
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-07-19 15:13:14 +0900

    Added support for Modified Hepburn romanization

--- lib/freegana.rb
+++ lib/freegana.rb
@@ -1,53 +1,101 @@
+# coding: utf-8
 require_relative "freegana/version"
 
 module Freegana
   class << self
-    VOWELS = ["a", "i", "u", "e", "o"]
-    CONSONANTS = ["x", "", "k", "g", "s", "z", "t", "d", "n", "h", "b", "p", "m", "r", "y", "w"]
+    HIRAGANA_START = 12353
+    KATAKANA_START = 12449
+    ROMAJI_EQUIVALENTS = [
+      "xa",  "a",
+      "xi",  "i",
+      "xu",  "u",
+      "xe",  "e",
+      "xo",  "o",
+      "ka", "ga",
+      "ki", "gi",
+      "ku", "gu",
+      "ke", "ge",
+      "ko", "go",
+      "sa", "za",
+      "si", "zi",
+      "su", "zu",
+      "se", "ze",
+      "so", "zo",
+      "ta", "da",
+      "ti", "di",
+      "xtu",
+      "tu", "du",
+      "te", "de",
+      "to", "do",
+      "na",
+      "ni",
+      "nu",
+      "ne",
+      "no",
+      "ha", "ba", "pa",
+      "hi", "bi", "pi",
+      "hu", "bu", "pu",
+      "he", "be", "pe",
+      "ho", "bo", "po",
+      "ma",
+      "mi",
+      "mu",
+      "me",
+      "mo",
+      "xya", "ya",
+      "xyu", "yu",
+      "xyo", "yo",
+      "ra",
+      "ri",
+      "ru",
+      "re",
+      "ro",
+      "xwa",
+      "wa",
+      "wi",
+      "we",
+      "wo",
+      "n'",
+      "vu"
+    ]
+    HIRAGANA_END = HIRAGANA_START + ROMAJI_EQUIVALENTS.length - 1
+    KATAKANA_END = KATAKANA_START + ROMAJI_EQUIVALENTS.length - 1
 
     def romanize(word)
       result = ""
       word.each_char do |char|
         result += convert_char(char)
       end
-      result
+      puts hepburn(result)
     end
 
     private
     
     def convert_char(char)
       ord = char.ord
-      if ord >= 12353 && ord <= 16000
-        if ord == 12387
-          return "xtsu"
-        elsif ord > 12387
-          ord -= 1
-        end
-        ord -= 12353
-
-        # Note: This is all integer math, so it rounds down
-        puts ord
-        if ord < 40
-          vowel_index = (ord % 10) / 2
-          consonant_index = 2 * (ord / 10) + (ord.odd? ? 1 : 0)
-        elsif ord < 45
-          vowel_index = ord % 5
-          consonant_index = 8
-        elsif ord < 60
-          vowel_index = (ord - 45) / 3
-          consonant_index = 9 + (ord % 3)
-        elsif ord < 70
-          vowel_index = ord % 5
-          consonant_index = 12 + (ord - 60) / 2
-        else
-          vowel_index = 0
-          consonant_index = 0
-        end
-
-        CONSONANTS[consonant_index] + VOWELS[vowel_index]
+      if    ord >= HIRAGANA_START && ord <= HIRAGANA_END
+        ROMAJI_EQUIVALENTS[ord - HIRAGANA_START]
+      elsif ord >= KATAKANA_START && ord <= KATAKANA_END
+        ROMAJI_EQUIVALENTS[ord - KATAKANA_START]
       else
         char
       end
+    end
+
+    def hepburn(word)
+      word.gsub(/aa/,  "ā")
+          .gsub(/iii/, "ī")
+          .gsub(/uu/,  "ū")
+          .gsub(/ee/,  "ē")
+          .gsub(/o[ou]/,  "ō")
+          .gsub(/si/, "shi")
+          .gsub(/[zd]i/, "ji")
+          .gsub(/ti/, "chi")
+          .gsub(/tu/,  "tsu")
+          .gsub(/du/, "zu")
+          .gsub(/hu/, "fu")
+          .gsub(/wo/, "o")
+          .gsub(/\'(?![aeiouy])/, "")
     end
   end
 end
